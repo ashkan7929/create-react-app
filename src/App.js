fc588cec7da44ef4b42d1680a7a45a5950ad0ab5
ashkan ansari <58354306+ashkan7929@users.noreply.github.com>
2022-08-28 17:06:21 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [isShow, setIsShow] = useState(false)
   setTimeout(() => {
     setIsShow(true)
-  }, 15000);
+  }, 5000);
   return (
     <div align='center' className="App">
       app is runing
@@ -14,15 +14,12 @@ function App() {
         <title>App Title</title>
         <meta property="og:title" content="The Rock" />
         <meta property="og:type" content="video.movie" />
-        <meta property="revised" content={new Date()} />
-
         <meta property="og:url" content="https://www.imdb.com/title/tt0117500/" />
         <meta property="og:image" content="https://ia.media-imdb.com/images/rock.jpg" />
-   
+        <meta name="revised" content={new Date()} />
         <meta property="og:description"
           content="Sean Connery found fame and fortune as the
            suave, sophisticated British agent, James Bond." />
-    <meta property=""
       </Helmet>}
     </div>
   );
