09b99644c009b6fea116cbfb563d0b0b78fe452c
ashkan ansari <58354306+ashkan7929@users.noreply.github.com>
2022-08-28 16:48:13 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { useState } from "react";
+import { Helmet } from "react-helmet";
 
 function App() {
+  const [isShow, setIsShow] = useState(false)
+  setTimeout(() => {
+    setIsShow(true)
+  }, 5000);
   return (
-      <div align='center' className="App">
-        <h1>Mamad baby</h1>
-      </div>
+    <div align='center' className="App">
+      app is runing
+      {isShow && <div>omaad</div>}
+      {isShow && <Helmet>
+        <title>App Title</title>
+        <meta property="og:title" content="The Rock" />
+        <meta property="og:type" content="video.movie" />
+        <meta property="og:url" content="https://www.imdb.com/title/tt0117500/" />
+        <meta property="og:image" content="https://ia.media-imdb.com/images/rock.jpg" />
+        <meta property="og:description"
+          content="Sean Connery found fame and fortune as the
+           suave, sophisticated British agent, James Bond." />
+      </Helmet>}
+    </div>
   );
 }
 
